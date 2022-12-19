ee7c684840312cb07d0d86346ed385e72e741a3c
ha0986 <ha0986@outlook.com>
2022-12-19 11:27:58 +0600

    beta1

--- app/src/main/java/com/hanif/tikfollow/login.java
+++ app/src/main/java/com/hanif/tikfollow/login.java
@@ -2,13 +2,42 @@ package com.hanif.tikfollow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button done = findViewById(R.id.done);
+        Button gues = findViewById(R.id.gp);
+
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(1==1){
+
+                }
+            }
+        });
+
+
+        gues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(login.this, profile.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
+
 }
