7b4cac9cec809468de54017279aa6fbb9e4e8a66
ha0986 <ha0986@outlook.com>
2022-12-20 21:45:54 +0600

    beta1

--- app/src/main/java/com/hanif/tikfollow/bonus.java
+++ app/src/main/java/com/hanif/tikfollow/bonus.java
@@ -2,13 +2,71 @@ package com.hanif.tikfollow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class bonus extends AppCompatActivity {
-
+public class bonus extends AppCompatActivity implements View.OnClickListener {
+    public String tag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bonus);
+
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+
     }
+
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.button1:
+                tag = "1";
+
+            case R.id.button2:
+                tag = "2";
+
+            case R.id.button3:
+                tag = "3";
+
+            case R.id.button4:
+                tag = "4";
+
+            case R.id.button5:
+                tag = "5";
+
+            case R.id.button6:
+                tag = "6";
+
+            case R.id.button7:
+                tag = "7";
+
+            case R.id.button8:
+                tag = "8";
+
+        }
+    }
+
+
 }
