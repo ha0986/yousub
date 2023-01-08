9fad76c342919088e7692363315dada955c9b9e0
ha0986 <ha0986@outlook.com>
2023-01-08 18:21:46 +0600

    final

--- app/src/main/java/com/hanif/likeefollow/jokes.java
+++ app/src/main/java/com/hanif/likeefollow/jokes.java
@@ -2,6 +2,9 @@ package com.hanif.likeefollow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,16 +16,18 @@ import java.util.ArrayList;
 
 public class jokes extends AppCompatActivity {
     int count = 0;
-
+    int size = 1;
+    ArrayList<String> jokelist = new ArrayList<>();
+    TextInputEditText jokes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes);
-        ArrayList<String> jokelist = new ArrayList<>();
+
         autoLoad.loadInter(this);
         autoLoad.loadReward(this,"");
         autoLoad.loadBanner(this,"top");
-        TextInputEditText jokes = findViewById(R.id.joke);
+        jokes = findViewById(R.id.joke);
 
         Button next = findViewById(R.id.next);
         Button prev  = findViewById(R.id.prev);
@@ -30,34 +35,46 @@ public class jokes extends AppCompatActivity {
 
 
 
+        getJokes();
+
+
         next.setOnClickListener(v -> {
-            count +=1;
-            jokelist.get(count);
+            if(count<size){
+                count +=1;
+                setJokes();
+            }else {
+                count = 1;
+                setJokes();
+            }
+
         });
 
+        prev.setOnClickListener(v -> {
+            if(count<2){
+                count = size-1;
+                setJokes();
+            }else {
+                count-=1;
+                setJokes();
+            }
+        });
 
+        copy.setOnClickListener(v -> {
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("jokes",jokelist.get(count));
+            clipboard.setPrimaryClip(clip);
+        });
 
     }
 
 
+    public void setJokes(){
+        jokes.setText(jokelist.get(count));
+    }
 
+    public void getJokes(){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 
